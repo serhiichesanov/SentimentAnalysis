3e49f7a92efa5778574d10207c735bcd790525f7
serhiichesanov <serhii.chesanov.knm.2020@edu.lpnu.ua>
2026-04-09 21:32:35 +0300

    Added logic for POST /api/prompts, GET /api/prompts

--- SentimentApp/views.py
+++ SentimentApp/views.py
@@ -1,3 +1,46 @@
-from django.shortcuts import render
+from SentimentApp.serializers import PromptSerializer, TaggingMetaSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Prompt
 
-# Create your views here.
+
+@api_view(['GET', 'POST'])
+def prompt(request):
+    if request.method == 'GET':
+        prompts = Prompt.objects.all()
+        serializer = PromptSerializer(prompts, many=True)
+
+        return Response(serializer, status=status.HTTP_200_OK)
+    else:
+        serializer = PromptSerializer(request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET'])
+def summary(request):
+    pass
+
+
+@api_view(['GET'])
+def topics(request):
+    pass
+
+
+@api_view(['GET'])
+def stages(request):
+    pass
+
+
+@api_view(['GET'])
+def insights(request):
+    pass
+
+
+
+
